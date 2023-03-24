f90862ebdf6ad3d1c17998d4a3e353832f7e3b2e
Kizito <kizitochiazor@gmail.com>
2023-03-24 03:25:21 +0100

    Task fix

---
+++ div.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * _div - function div two number
+ * @stack: Stack list
+ * @line_number: Number of the line
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curerent = NULL;
+	int div = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	curerent = *stack;
+	div = curerent->next->n / curerent->n;
+	curerent->next->n = div;
+	_pop(stack, line_number);
+
+}
+
---
+++ sub.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * _sub - subtracts the top element of the stack from the second.
+ * @stack: Stack list
+ * @line_number: Number of line
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	int sub = 0;
+
+	if (!current || !current->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	sub = current->next->n - current->n;
+	current->next->n = sub;
+	_pop(stack, line_number);
+}
